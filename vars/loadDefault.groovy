cb72ff52e6130fbc961112c03ad662572bc6534f
LEE JEONG WOOK <tarpha@gmail.com>
2023-04-06 23:53:35 +0900

    test

--- vars/loadDefault.groovy
+++ vars/loadDefault.groovy
@@ -1,29 +1,39 @@
 def call(String name) {
-    node {
-        stage('Clone repository') {
-            checkout scm
+    pipeline {
+        environment { 
+            repository = "ghcr.io/tarpha/$JOB_NAME"  //docker hub id와 repository 이름
+            dockerImage = '' 
         }
-        stage('Build image') {
-            app = docker.build("ghcr.io/tarpha/${env.JOB_NAME}")
-        }
-        stage('Push image') {
-            docker.withRegistry('https://ghcr.io/tarpha', 'ghcr') {
-            app.push("${env.BUILD_NUMBER}")
-            // app.push("0.0.1")
+        agent any
+        stages {
+            stage('Clone repository') {
+                steps {
+                    checkout scm
+                }
+            }
+            stage('Build image') {
+                steps {
+                    script { 
+                        dockerImage = docker.build repository + ":$BUILD_NUMBER" 
+                    }
+                }
+            }
+            stage('Push image') {
+                steps {
+                    script {
+                        docker.withRegistry('https://ghcr.io/tarpha', 'ghcr') {
+                            dockerImage.push("${env.BUILD_NUMBER}")
+                            dockerImage.push("0.0.1")
+                        }
+                    }
+                }
             }
         }
-        stage('Clean') {
-            /* clean up our workspace */
-            deleteDir()
-            /* clean up tmp directory */
-            dir("${workspace}@tmp") {
-                deleteDir()
+        post {
+            always {
+                cleanWs()
+                echo "END ${name}"
             }
-            /* clean up script directory */
-            dir("${workspace}@script") {
-                deleteDir()
-            }
-            echo "End ${name}"
         }
     }
 }
